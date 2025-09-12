1b754f53bfc5a6916d1bf9e82625f966e31a98d9
joachima2008 <joachima2008@gmail.com>
2025-09-12 14:45:24 +0000

    refactor: improve project structure and testing

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,9 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   roots: [
-    '<rootDir>/src',
     '<rootDir>/tests'
   ],
   testMatch: [
-    '**/__tests__/**/*.ts',
     '**/?(*.)+(spec|test).ts'
   ],
   collectCoverageFrom: [
