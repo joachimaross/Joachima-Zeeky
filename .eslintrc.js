d08a98b6b6493e7842ecc6c431a613c571bce14b
Joachima Ross <jacameno@outlook.com>
2025-09-13 11:25:56 -0500

    feat: refactor web server and improve project structure

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,13 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   plugins: ['@typescript-eslint/eslint-plugin'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'warn',
   },
 };
