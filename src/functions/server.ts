d24c7d2a927283310e5f6338c731068cec35635e
joachima2008 <joachima2008@gmail.com>
2025-09-13 14:40:19 +0000

    Fix: remove module-alias from serverless function

--- src/functions/server.ts
+++ src/functions/server.ts
@@ -1,8 +1,7 @@
-import 'module-alias/register';
-import serverless from 'serverless-http';
-import { ZeekyApplication } from '../ZeekyApplication';
-import { APIGatewayProxyEvent, Context } from 'aws-lambda';
-import { Application } from 'express';
+import serverless from "serverless-http";
+import { ZeekyApplication } from "../ZeekyApplication";
+import { APIGatewayProxyEvent, Context } from "aws-lambda";
+import { Application } from "express";
 
 let expressApp: Application;
 
@@ -15,7 +14,10 @@ async function bootstrap(): Promise<Application> {
   return expressApp;
 }
 
-export const handler = async (event: APIGatewayProxyEvent, context: Context) => {
+export const handler = async (
+  event: APIGatewayProxyEvent,
+  context: Context,
+) => {
   const app = await bootstrap();
   return serverless(app)(event, context);
 };
